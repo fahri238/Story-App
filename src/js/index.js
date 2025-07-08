721b18b8fc64664265d86d780aa78761b3c8a51f
fahrilmi25 <fahrilmi25@gmail.com>
2025-07-08 15:28:03 +0800

    Fix: Perbaiki tampilan peta dan logika update

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "regenerator-runtime";
 import "../public/css/style.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "leaflet/dist/leaflet.css";
 
 import App from "./app.js";
 import router from "./routes/router.js";
